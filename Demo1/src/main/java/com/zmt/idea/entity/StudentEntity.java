f57fa5d09363963de54643208b32227821a092d8
zmt <13627234120@163.com>
2017-10-11 17:04:14 +0800

    Initial commit

---
+++ Demo1/src/main/java/com/zmt/idea/entity/StudentEntity.java
@@ -0,0 +1,49 @@
+package com.zmt.idea.entity;
+
+public class StudentEntity {
+    private int stuid;
+    private String stuname;
+    private String stusex;
+    private int stuage;
+    private String stuaddr;
+
+    public int getStuid() {
+        return stuid;
+    }
+
+    public void setStuid(int stuid) {
+        this.stuid = stuid;
+    }
+
+    public String getStuname() {
+        return stuname;
+    }
+
+    public void setStuname(String stuname) {
+        this.stuname = stuname;
+    }
+
+    public String getStusex() {
+        return stusex;
+    }
+
+    public void setStusex(String stusex) {
+        this.stusex = stusex;
+    }
+
+    public int getStuage() {
+        return stuage;
+    }
+
+    public void setStuage(int stuage) {
+        this.stuage = stuage;
+    }
+
+    public String getStuaddr() {
+        return stuaddr;
+    }
+
+    public void setStuaddr(String stuaddr) {
+        this.stuaddr = stuaddr;
+    }
+}
